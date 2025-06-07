d30d96da1bdf4bff4bf1b2dd4ba8a0b1b7ebd5c5
TM64715 <tage.mehta@gmail.com>
2025-06-07 11:52:19 -0400

    Initial Commit

---
+++ thing-finder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  thing-finder
+//
+//  Created by Sam Mehta on 6/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
